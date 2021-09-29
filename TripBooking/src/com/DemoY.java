7de64e1237301748ea106e9a70b56a2f6a8e69e5
Amruta <amrutavarute22@gmail.com>
2021-09-29 16:32:09 -0700

    made chnages to develop branch code

--- TripBooking/src/com/DemoY.java
+++ TripBooking/src/com/DemoY.java
@@ -10,6 +10,14 @@ public class DemoY {
 		System.out.println("i have made changes to Y");
 		System.out.println(c);
 		System.out.println("abc");
+		
+		int a1=10;
+		int b1=5;
+		int c1=a1/b1;
+		System.out.println(c1);
+		System.out.println("i have made changes to Y");
+		System.out.println(c1);
+		System.out.println("abc");
 
 	}
 

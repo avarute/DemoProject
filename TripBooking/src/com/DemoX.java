4f51fbc2a7e68c214d0bb9112ebe05c53ac58efe
Amruta <amrutavarute22@gmail.com>
2021-09-29 09:25:38 -0700

    commited to demoProject

---
+++ TripBooking/src/com/DemoX.java
@@ -0,0 +1,13 @@
+package com;
+
+public class DemoX {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=5;
+		int c=a+b;
+		System.out.println(c);
+		System.out.println("GIT DEMO CHANGES");
+	}
+
+}
---
+++ TripBooking/src/com/DemoY.java
@@ -0,0 +1,14 @@
+package com;
+
+public class DemoY {
+
+	public static void main(String[] args) {
+		int a=2;
+		int b=5;
+		int c=a*b;
+		System.out.println(c);
+		System.out.println("i have made changes to Y");
+
+	}
+
+}
